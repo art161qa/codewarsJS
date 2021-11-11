4f544b01ca74278b833b1b75167f27a044a823da
Artem Razinkin <art.razinkin@gmail.com>
2021-11-11 19:52:10 +0300

    Added highestAndLowest.js

---
+++ highestAndLowest.js
@@ -0,0 +1,15 @@
+numb = "8 3 -5 42 -1 0 0 -9 4 7 4 -4"
+
+function highAndLow(numbers){
+    let arrNum = numbers.split(' ')
+    let min = arrNum[0]
+    let max = arrNum[0]
+    let result = ""
+    for (let i = 0; i < arrNum.length; i++){
+        if (+min > +arrNum[i]) min = arrNum[i];
+        if (+max < +arrNum[i]) max = arrNum[i]
+    }
+    result += `${max} ${min}`
+    return result
+  }
+  console.log(highAndLow(numb))
