26f3d9f8034f8bb87ebc85f7c9e1d38978918d2b
Artem Razinkin <art.razinkin@gmail.com>
2021-11-28 02:25:32 +0300

    Added countVowel.js

---
+++ countVowel.js
@@ -0,0 +1,13 @@
+let strinf = 'aaqwed'
+function getCount(str) {
+    var vowelsCount = 0;
+    let vowels = ['a','e','i','o','u']
+    for (let i = 0; i < str.length; i++){
+        if (vowels.indexOf(str[i]) != -1){
+            vowelsCount++
+        }
+    }
+    
+    return vowelsCount;
+  }
+  console.log(getCount(strinf))
