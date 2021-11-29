9c4528dd677e6e454051cfb9a8eb5e971d042936
Artem Razinkin <art.razinkin@gmail.com>
2021-11-30 00:53:22 +0300

    Added link to kata uniqueInOrder.js

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -7,6 +7,8 @@ uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
+https://www.codewars.com/kata/54e6533c92449cc251001667/javascript
+
 */
 let someStr = 'aabcdeb'
 var uniqueInOrder=function(iterable){
